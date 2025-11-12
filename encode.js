95b0e6936a1446bbbacde74ce711820d2bb66058
Asif Mahmud <149937516+am-mahmud@users.noreply.github.com>
2025-11-12 18:06:16 -0500

    Update encode.js

--- encode.js
+++ encode.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-const key = fs.readFileSync("./ums-auth-firebase-adminsdk.json", "utf8");
+const key = fs.readFileSync("./ums-auth-firebase-admin.json", "utf8");
 const base64 = Buffer.from(key).toString("base64");
 console.log(base64);
